f39c6c8b5779f1b6c6b10dd306bf248e09fe50f2
zsoltibv <zsoltibv@gmail.com>
2025-10-20 00:10:57 +0300

    feat: add role request model

---
+++ Evento.Domain/Enums/RequestStatus.cs
@@ -0,0 +1,8 @@
+﻿namespace Evento.Domain.Enums;
+
+public enum RequestStatus
+{
+    Pending = 0,
+    Approved = 1,
+    Rejected = 2
+}
---
+++ Evento.Domain/Models/RoleRequest.cs
@@ -0,0 +1,18 @@
+﻿using Evento.Domain.Enums;
+
+namespace Evento.Domain.Models;
+
+public class RoleRequest
+{
+    public int Id { get; set; } 
+
+    public string UserId { get; set; } = null!; 
+    public AppUser User { get; set; } = null!;
+
+    public string RoleName { get; set; } = null!; // Admin, VenueAdmin
+    public RequestStatus Status { get; set; } = RequestStatus.Pending; // Pending, Approved, Rejected
+    public DateTime RequestDate { get; set; } = DateTime.UtcNow;
+    
+    public int? VenueId { get; set; } // only set if RoleName == "VenueAdmin"
+    public Venue? Venue { get; set; }
+}
